0b59030cefee972543e274c16a4df3048c7ecd8a
Sumithra <sumisooriya@gmail.com>
2022-09-08 15:52:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
 import logo from './logo.svg';
+  import { useEffect, useState } from "react";
 import './App.css';
-
+import Navbar from "./Navbar"
+const users = [
+  {
+    username: 'admin1',
+    password: '12345678'
+  },
+  {
+    username:'admin2',
+    password:'012345678'
+  }
+];
 function App() {
+  const [data, setData] = useState({
+    username: '',
+    password: ''
+  });
+  const changeHandler = (e) => {
+    setData({...data, [e.target.name]: e.target.value})
+  }
+
+  const checkUser = () => {
+    const usercheck = users.find(user => (user.username === data.username && user.password === data.password));
+    if(usercheck) {
+      console.log("Login successful");
+    }else {
+      console.log("Wrong password or username");
+    }
+    // console.log(uname);
+    console.log(usercheck);
+  }
+
+
+  useEffect(() => {
+checkUser(users)
+  }, [data.username, data.password])
+
+  console.log(data)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           Enter your pin please
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+         <div className="input-text">
+              <input
+                type="text"
+                name="username"
+                value={data.username}
+                placeholder="Username"
+                aria-describedby="inputGroupPrepend2" required
+                onChange={changeHandler}
+              />
+          </div>
+          <div className="input-text">
+            <input
+              type="password"
+              name="password"
+              value={data.password}
+              placeholder="Password"
+              aria-describedby="inputGroupPrepend2" required
+              onChange={changeHandler}
+            />
+            
+          </div>
+        <button type="submit" >Login</button>
       </header>
+       
     </div>
   );
 }
